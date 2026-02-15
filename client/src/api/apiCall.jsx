1a885defab7c7ecbc1e0baf7be1187bba5ab0b09
Mahar Abdul Qudoos <aad769650@gmail.com>
2026-02-15 14:18:51 +0500

    Update API endpoints to use production URL

--- client/src/api/apiCall.jsx
+++ client/src/api/apiCall.jsx
@@ -6,7 +6,7 @@ export const apiCall=async (actualData) => {
     console.log(actualData);
     // for (let [key, val] of actualData.entries()) console.log(key, val)
 
-    const resp=await axios.post("http://localhost:8000/host/detail",actualData)
+    const resp=await axios.post("https://rentra-mern.onrender.com/host/detail",actualData)
     console.log("resp",resp);
     
 }
@@ -14,7 +14,7 @@ export const apiCall=async (actualData) => {
 
 
 export const getAllData=async()=>{
-       const resp=await axios.get("http://localhost:8000/host/detail")
+       const resp=await axios.get("https://rentra-mern.onrender.com/host/detail")
     console.log("resp",resp.data.msg[0]);
     return resp.data.msg[0];
 }
@@ -25,7 +25,7 @@ export const registerHost=async(actualData)=>{
      for (let [key, val] of actualData.entries()) console.log(key, val)
 
    try {
-     const resp=await axios.post("http://localhost:8000/owner/register",actualData,{withCredentials:true})
+     const resp=await axios.post("https://rentra-mern.onrender.com/owner/register",actualData,{withCredentials:true})
     console.log("resp",resp);
     return  resp
    } catch (error) {
@@ -41,7 +41,7 @@ export const verifyHostLogin=async(formData)=>{
    try {
     console.log(formData);
    
-     const resp=await axios.post("http://localhost:8000/owner/login",formData,{withCredentials:true})
+     const resp=await axios.post("https://rentra-mern.onrender.com/owner/login",formData,{withCredentials:true})
     console.log("resp",resp);
   
     return  resp
@@ -55,7 +55,7 @@ export const verifyHostLogin=async(formData)=>{
 
 export const AccessDashBoard=async()=>{
 try {
-     const response=axios.get("http://localhost:8000/owner/dashBoard",{withCredentials:true})
+     const response=axios.get("https://rentra-mern.onrender.com/owner/dashBoard",{withCredentials:true})
      .then((resp)=>{
     console.log("resp",resp);
 
@@ -64,7 +64,7 @@ try {
     if (resp.status==204) {
       console.log("refreshing...");
       
-      axios.get("http://localhost:8000/refresh",{withCredentials:true})
+      axios.get("https://rentra-mern.onrender.com/refresh",{withCredentials:true})
       .then((data)=>{
          if(data.status==204){
             toast.info("please Login")
@@ -99,7 +99,7 @@ try {
 
 
 export const getProfileInfo=async()=>{
-          const resp=await axios.get("http://localhost:8000/owner/profileInfo",{withCredentials:true})
+          const resp=await axios.get("https://rentra-mern.onrender.com/owner/profileInfo",{withCredentials:true})
     console.log("resp",resp.data.msg[0]);
     return resp.data.msg[0];
 }
@@ -107,24 +107,24 @@ export const getProfileInfo=async()=>{
 
 
 export const getAllPostData=async()=>{
-          const resp=await axios.get("http://localhost:8000/owner/AllPost",{withCredentials:true})
+          const resp=await axios.get("https://rentra-mern.onrender.com/owner/AllPost",{withCredentials:true})
     console.log("resp",resp.data);
     return resp.data;
 }
 
 
 export const ApproveRoom=async(id)=>{
-        const resp=await axios.patch("http://localhost:8000/owner/updateStatus",{id},{withCredentials:true})
+        const resp=await axios.patch("https://rentra-mern.onrender.com/owner/updateStatus",{id},{withCredentials:true})
     console.log("resp",resp.data);
     return resp.data;
 }
 export const RejectRoom=async(id)=>{
-        const resp=await axios.patch("http://localhost:8000/owner/updateStatusReject",{id},{withCredentials:true})
+        const resp=await axios.patch("https://rentra-mern.onrender.com/owner/updateStatusReject",{id},{withCredentials:true})
     console.log("resp",resp.data);
     return resp.data;
 }
 export const DeleteRoom=async(id)=>{
-        const resp=await axios.delete("http://localhost:8000/owner/delete",{id},{withCredentials:true})
+        const resp=await axios.delete("https://rentra-mern.onrender.com/owner/delete",{id},{withCredentials:true})
     console.log("resp",resp.data);
     return resp.data;
 }
@@ -132,7 +132,7 @@ export const DeleteRoom=async(id)=>{
 
 
 export const Logout=async()=>{
-        const resp=await axios.get("http://localhost:8000/owner/logout",{withCredentials:true})
+        const resp=await axios.get("https://rentra-mern.onrender.com/owner/logout",{withCredentials:true})
     console.log("resp",resp.data);
     return resp.data;
 }
@@ -141,7 +141,7 @@ export const Logout=async()=>{
 export const sendMailToAdmin=async(data)=>{
     console.log(data);
     
-        const resp=await axios.post("http://localhost:8000/sendMail",{data},{withCredentials:true})
+        const resp=await axios.post("https://rentra-mern.onrender.com/sendMail",{data},{withCredentials:true})
     console.log("resp",resp.data);
     return resp.data;
 }
